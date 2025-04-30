d62dcfb593934ed8d4d10ee74e7adfe22de6801b
junaJuned <arjunacandra342@gmail.com>
2025-05-01 00:49:27 +0700

    Init Project

---
+++ src/main/java/id/uver/school/FreeSisApplication.java
@@ -0,0 +1,13 @@
+package id.uver.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeSisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeSisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/uver/school/FreeSisApplicationTests.java
@@ -0,0 +1,13 @@
+package id.uver.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeSisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
